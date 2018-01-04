67256c51389971ead9f2a954a242b6bef9f417a2
dongjiren <jiren.dong@uhouzz.com>
2018-01-04 10:37:57 +0800

    fetch完成

--- src/fetch/data.js
+++ src/fetch/data.js
@@ -1,47 +1,31 @@
-// import { get } from './get.js';
-// import { post } from './post.js';
-
-// export function getData() {
-//     console.log('GET方法');
-//     var result = get('/webapp1.0/index.php/AirTicket/car/searchAirport?searchKey=纽约&src=91_h5');
-
-//     result.then(res => {
-//         return res.json()
-//     }).then(json => {
-//         console.log(json)
-//     })
-// };
-
-// export function postData() {
-//     // '/api/post' 提交数据
-//     // var result = post('/api/post', {
-//     //     a: 100,
-//     //     b: 200
-//     // })
-
-//     // result.then(res => {
-//     //     return res.json()
-//     // }).then(json => {
-//     //     console.log(json)
-//     // })
-//     console.log('POST方法');
-// };
-
-
-import 'whatwg-fetch'
-import 'es6-promise'
+import md5 from 'js-md5';
+import { get } from './get.js';
+import { post } from './post.js';
 
 export function getData() {
-    fetch('/index.php/AirTicket/car/searchAirport?searchKey=纽约&src=91_h5')
-    .then(res => {
+    console.log('GET方法');
+    var result = get('/index.php/AirTicket/car/searchAirport?searchKey=纽约&src=91_h5');
+
+    result.then(res => {
         return res.json()
-    }).then(data => {
-        console.log('data >', data)
-    }).catch(error => {
-        console.log('error >', error)
+    }).then(json => {
+        console.log(json)
     })
-}
+};
 
 export function postData() {
     console.log('POST方法');
-}
+    var result = post('/index.php/buser/user/login', {
+        countryCode: '+86',
+        password: md5('d12345'),
+        username: '18911281024',
+        src: '91_h5'
+    })
+
+    result.then(res => {
+        return res.json()
+    }).then(json => {
+        console.log(json)
+    })
+    
+};
